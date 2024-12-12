227308f500c3a234332bc6fb5778e73736f2f925
unknown <dineshsarathi0402@gmail.com>
2024-12-12 13:23:56 +0530

    11th commit 4.0

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const superAdminRoute = require('./routes/superAdminRoute') // Import your routes
 const templateRouter = require('./routes/templateRouter')// Import your routes
 const taskRoutes = require('./routes/taskReports')
+const bugreport = require('./routes/bugreport')
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -25,6 +26,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/superadmin', superAdminRoute);
 app.use('/api/template', templateRouter);
 app.use('/api/taskreports', taskRoutes);
+app.use('/api/bugreport',bugreport)
 // Start the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`,process.env.MONGO_URI));
